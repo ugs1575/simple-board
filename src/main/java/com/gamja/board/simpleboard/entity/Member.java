3dd07136c536bd2836ac5f77821968c6ebf8155b
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2023-05-26 17:49:47 +0900

    feat: entity 생성

---
+++ src/main/java/com/gamja/board/simpleboard/entity/Member.java
@@ -0,0 +1,30 @@
+package com.gamja.board.simpleboard.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "member_id")
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Builder
+	public Member(String name) {
+		this.name = name;
+	}
+}
---
+++ src/main/java/com/gamja/board/simpleboard/entity/Post.java
@@ -0,0 +1,43 @@
+package com.gamja.board.simpleboard.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "post_id")
+	private Long id;
+
+	@Column(length = 500, nullable = false)
+	private String title;
+
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String text;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+
+	@Builder
+	public Post(Long id, String title, String text, Member member) {
+		this.id = id;
+		this.title = title;
+		this.text = text;
+		this.member = member;
+	}
+}
