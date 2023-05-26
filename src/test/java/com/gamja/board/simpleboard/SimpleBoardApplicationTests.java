128bcfbd8b157985b84ea73309ef253131deefa7
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2023-05-26 17:41:51 +0900

    feat: 프로젝트 세팅 & application.yml 파일 추가

---
+++ src/test/java/com/gamja/board/simpleboard/SimpleBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamja.board.simpleboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
