827148d78032fbc9a4c5586ace8514d79a9e621b
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2023-05-28 16:00:38 +0900

    test: 게시글 생성 api 테스트 코드 작성

---
+++ src/test/java/com/gamja/board/simpleboard/controller/PostApiControllerTest.java
@@ -0,0 +1,84 @@
+package com.gamja.board.simpleboard.controller;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.gamja.board.simpleboard.dto.PostSaveRequestDto;
+import com.gamja.board.simpleboard.entity.Member;
+import com.gamja.board.simpleboard.entity.Post;
+import com.gamja.board.simpleboard.repository.MemberRepository;
+import com.gamja.board.simpleboard.repository.PostRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PostApiControllerTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Autowired
+	private PostRepository postRepository;
+
+	private Member member;
+
+	@BeforeEach
+	public void setUp() {
+		member = memberRepository.save(Member.builder()
+			.name("우경서")
+			.build());
+	}
+
+	@AfterEach
+	public void afterEach() {
+		postRepository.deleteAll();
+		memberRepository.deleteAll();
+	}
+
+
+	@Test
+	public void 게시글을_작성한다() throws Exception {
+	    //given
+		String expectedTitle = "제목1";
+		String expectedContent = "테스트";
+
+		PostSaveRequestDto requestDto = PostSaveRequestDto.builder()
+															.title(expectedTitle)
+															.content(expectedContent)
+															.build();
+
+		String url = "http://localhost:" + port + "/api/members/" + member.getId() + "/posts";
+
+	    //when
+		ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+	    //then
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+		assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+		List<Post> all = postRepository.findAll();
+		assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+		assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+		assertThat(all.get(0).getMember().getId()).isEqualTo(member.getId());
+	}
+
+
+
+
+
+}
