1bd807d5491e6431ee3cb299f392ddf2e5d5bf66
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2023-05-28 14:11:30 +0900

    test: 회원 수정 api 테스트 코드 추가

--- src/test/java/com/gamja/board/simpleboard/controller/MemberApiControllerTest.java
+++ src/test/java/com/gamja/board/simpleboard/controller/MemberApiControllerTest.java
@@ -4,21 +4,23 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.gamja.board.simpleboard.dto.MemberSaveRequestDto;
+import com.gamja.board.simpleboard.dto.MemberUpdateRequestDto;
 import com.gamja.board.simpleboard.entity.Member;
 import com.gamja.board.simpleboard.repository.MemberRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Transactional
 class MemberApiControllerTest {
 
 	@LocalServerPort
@@ -29,6 +31,12 @@ class MemberApiControllerTest {
 
 	@Autowired
 	private MemberRepository memberRepository;
+
+	@AfterEach
+	public void afterEach() {
+		memberRepository.deleteAll();
+	}
+
 
 	@Test
 	public void 회원이_등록된다() throws Exception {
@@ -51,5 +59,36 @@ class MemberApiControllerTest {
 		List<Member> all = memberRepository.findAll();
 		assertThat(all.get(0).getName()).isEqualTo(name);
 	}
+
+	@Test
+	public void 회원_정보를_수정한다() throws Exception {
+		//given
+		Member saveMember = memberRepository.save(Member.builder()
+													.name("우경서")
+													.build());
+
+		Long id = saveMember.getId();
+		String expectedName = "우경서2";
+
+		MemberUpdateRequestDto requestDto = MemberUpdateRequestDto.builder()
+																.name(expectedName)
+																.build();
+
+		String url = "http://localhost:" + port + "/api/members/" + id;
+
+		HttpEntity<MemberUpdateRequestDto> requestHttpEntity = new HttpEntity<>(requestDto);
+
+		//when
+		ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PATCH, requestHttpEntity, Long.class);
+
+		//then
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(responseEntity.getBody()).isEqualTo(id);
+
+		List<Member> all = memberRepository.findAll();
+		assertThat(all.get(0).getName()).isEqualTo(expectedName);
+	}
+
+
 
 }
