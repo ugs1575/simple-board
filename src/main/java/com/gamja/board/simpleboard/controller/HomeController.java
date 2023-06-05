ff0e155c531c0b63b2a0fff11f015db3f0c1a09e
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2023-06-05 18:06:51 +0900

    feat: 게시판 레이아웃 화면 추가

---
+++ src/main/java/com/gamja/board/simpleboard/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.gamja.board.simpleboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping
+	public String index() {
+		return "index";
+	}
+}
