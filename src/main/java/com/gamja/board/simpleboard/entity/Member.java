7d2e44621ac8633ce32066c36a180d74e7afbc66
우경서 (Gyeongseo Woo) <ugs1575@gmail.com>
2023-06-16 20:17:52 +0900

    feat: 회원 삭제 api soft delete로 변경

--- src/main/java/com/gamja/board/simpleboard/entity/Member.java
+++ src/main/java/com/gamja/board/simpleboard/entity/Member.java
@@ -5,6 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
 import com.gamja.board.simpleboard.common.domain.BaseTimeEntity;
 
 import lombok.AccessLevel;
@@ -12,6 +15,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@SQLDelete(sql = "UPDATE member SET deleted = true WHERE member_id=?")
+@Where(clause = "deleted=false")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
@@ -24,6 +29,9 @@ public class Member extends BaseTimeEntity {
 
 	@Column(nullable = false)
 	private String name;
+
+	@Column(nullable = false, columnDefinition = "boolean default false")
+	private boolean deleted;
 
 	@Builder
 	public Member(String name) {
